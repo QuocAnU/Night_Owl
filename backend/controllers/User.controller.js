31a31d324663b2ce8a1916e638a6b0f68550655e
QuocAnU <an.lequoc2103@hcmut.edu.vn>
2024-09-12 19:30:08 +0700

    first commit

--- backend/controllers/User.controller.js
+++ backend/controllers/User.controller.js
@@ -1,4 +1,4 @@
-const User = require('../models/User.js');
+const User = require('./../models/User');
 const UserDiscount = require('../models/UserDiscount');
 
 const createUser = async (req, res) => {
